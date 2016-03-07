9710dbf21bc5c666793da2ff3c468a9c84f4925d
Andrew Schaffer <Andrew@Andrews-MacBook-2.local>
2016-03-06 21:16:36 -0500

    sign in does not authenticate if already logged in

--- www/js/controllers/gauth.controller.js
+++ www/js/controllers/gauth.controller.js
@@ -9,11 +9,17 @@
     function GAuthController($scope, Drive, $state) {
         $scope.loginByGoogle = function() {
 
+            var auth_token = gapi.auth.getToken();
+            if(auth_token) {
+                $state.go('app.drive');
+            }
+            
+            else  {
             var client_id = "775512295394-hhg8etqdcmoc8i7r5a6m9d42d4ebu63d.apps.googleusercontent.com"; //web-app
             var scopes = ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/userinfo.email'];
             Drive.authenticate(client_id, scopes, {
                 redirect_uri: 'http://localhost/callback/'
-            })
+                })
                 .then(function(response) { //authenticate
                         if (response) {
                             var token = response.access_token;
@@ -24,6 +30,7 @@
                     function(error) {
                         console.log("" + error);
                     });
+            }
         };
 
         var vm = this;
