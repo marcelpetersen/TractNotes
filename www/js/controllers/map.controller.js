31bfca9581317f3af85c6b5bbd14085c201fcca7
water42 <sdemurjian@gmail.com>
2016-03-17 20:13:38 -0400

    area/distance abstracted to factory
    Closes #23

--- www/js/controllers/map.controller.js
+++ www/js/controllers/map.controller.js
@@ -27,12 +27,9 @@
         vm.createMarker = createMarker;
         vm.record = record;
         vm.drawInit = drawInit;
-        vm.showPolygonArea = showPolygonArea;
-        vm.showPolygonAreaEdited = showPolygonAreaEdited;
         vm.xmldata = xmldata;
         vm.saveTrack = saveTrack;
         vm.discardTrack = discardTrack;
-
 
         //@todo change events to factory listeners
         /** @listens $rootScope.Import */
@@ -157,69 +154,8 @@
             vm.drawnItems = drawnItemsService.getDrawnItems();
             vm.map.addLayer(vm.drawnItems);
             vm.layercontrol.addOverlay(vm.drawnItems, 'Drawn items', 'Other');
-            vm.map.on('draw:created', showPolygonArea);
-            vm.map.on('draw:edited', showPolygonAreaEdited);
-        }
-
-        /**
-         * Recalculate area/length of each layer on edit.
-         * @function
-         * @param {object} layer
-         */
-        function showPolygonAreaEdited(e) {
-            e.layers.eachLayer(function(layer) {
-                showPolygonArea({
-                    layer: layer
-                });
-            });
-        }
-
-        /**
-         * Show area/length of each layer on created.
-         * @function
-         * @author https://stackoverflow.com/questions/31221088/how-to-calculate-the-distance-of-a-polyline-in-leaflet-like-geojson-io
-         * @param {object} layer
-         * @todo Abstract into drawnItems.factory.js
-         */
-        function showPolygonArea(e) {
-            var type = e.layerType;
-            var layer = e.layer;
-            if (type === 'polyline') {
-                drawnItemsService.addToDrawnItems(layer);
-                var tempLatLng = e.layer._latlngs[0];
-                var totalDistance = 0.00000;
-                console.log(e.layer._latlngs.length)
-                for (var i = 0; i < e.layer._latlngs.length; i++){
-                    var latlng = e.layer._latlngs[i];
-                    totalDistance += tempLatLng.distanceTo(latlng);
-                    tempLatLng = latlng;
-                }
-                e.layer.bindPopup((totalDistance).toFixed(2) + ' meters');
-                e.layer.openPopup();
-            } else if (type === 'circle') {
-                drawnItemsService.addToDrawnItems(layer);
-                var area = 0;
-                var radius = e.layer.getRadius();
-                area = (Math.PI) * (radius * radius);
-                e.layer.bindPopup((area / 1000000).toFixed(2) + ' km<sup>2</sup>');
-                e.layer.openPopup();
-            } else if (type === 'polygon' || type === 'rectangle') {
-                drawnItemsService.addToDrawnItems(layer);
-                e.layer.bindPopup((LGeo.area(e.layer) / 1000000).toFixed(2) + ' km<sup>2</sup>');
-                e.layer.openPopup();
-            } else if (type === 'marker') {
-                drawnItemsService.addToDrawnItems(layer);
-                var newLoc = layer.getLatLng();
-                var currentPosition = locationService.locate();
-                currentPosition.then(function(val) {
-                    var lat = val.position.coords.latitude;
-                    var long = val.position.coords.longitude;
-                    e.layer.bindPopup((newLoc.distanceTo([lat, long])).toFixed(2) + 'm from current position.');
-                });
-            } else {
-                drawnItemsService.addToDrawnItems(layer);
-            }
-
+            vm.map.on('draw:created', drawnItemsService.showPolygonArea);
+            vm.map.on('draw:edited', drawnItemsService.showPolygonAreaEdited);
         }
 
         function xmldata(layer) {
--- www/js/services/drawnItems.factory.js
+++ www/js/services/drawnItems.factory.js
@@ -13,7 +13,9 @@
 
         var service = {
             addToDrawnItems: addToDrawnItems,
-            getDrawnItems: getDrawnItems
+            getDrawnItems: getDrawnItems,
+            showPolygonArea: showPolygonArea,
+            showPolygonAreaEdited: showPolygonAreaEdited
         };
         return service;
 
@@ -25,6 +27,65 @@
 
         function getDrawnItems() {
             return drawnItems;
+        }
+
+        /**
+         * Show area/length of each layer on created.
+         * @function
+         * @author https://stackoverflow.com/questions/31221088/how-to-calculate-the-distance-of-a-polyline-in-leaflet-like-geojson-io
+         * @param {object} layer
+         */
+        function showPolygonArea(e) {
+            var type = e.layerType;
+            var layer = e.layer;
+            if (type === 'polyline') {
+                addToDrawnItems(layer);
+                var tempLatLng = e.layer._latlngs[0];
+                var totalDistance = 0.00000;
+                for (var i = 0; i < e.layer._latlngs.length; i++) {
+                    var latlng = e.layer._latlngs[i];
+                    totalDistance += tempLatLng.distanceTo(latlng);
+                    tempLatLng = latlng;
+                }
+                e.layer.bindPopup((totalDistance).toFixed(2) + ' meters');
+                e.layer.openPopup();
+            } else if (type === 'circle') {
+                addToDrawnItems(layer);
+                var area = 0;
+                var radius = e.layer.getRadius();
+                area = (Math.PI) * (radius * radius);
+                e.layer.bindPopup((area / 1000000).toFixed(2) + ' km<sup>2</sup>');
+                e.layer.openPopup();
+            } else if (type === 'polygon' || type === 'rectangle') {
+                addToDrawnItems(layer);
+                e.layer.bindPopup((LGeo.area(e.layer) / 1000000).toFixed(2) + ' km<sup>2</sup>');
+                e.layer.openPopup();
+            } else if (type === 'marker') {
+                addToDrawnItems(layer);
+                var newLoc = layer.getLatLng();
+                var currentPosition = locationService.locate();
+                currentPosition.then(function(val) {
+                    var lat = val.position.coords.latitude;
+                    var long = val.position.coords.longitude;
+                    e.layer.bindPopup((newLoc.distanceTo([lat, long])).toFixed(2) + 'm from current position.');
+                });
+            } else {
+                addToDrawnItems(layer);
+            }
+        }
+
+
+        /**
+         * Recalculate area/length of each layer on edit.
+         * @function
+         * @param {object} layer
+         */
+        function showPolygonAreaEdited(e) {
+            e.layers.eachLayer(function(layer) {
+                showPolygonArea({
+                    layer: layer
+                });
+            });
         }
     }
 })();
