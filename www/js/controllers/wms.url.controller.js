e024c0441d3eb0bbc3590279ce02a632c52f3b44
William <william.stewart_iii@uconn.edu>
2016-04-06 21:37:38 -0400

    Added placeholder text change to "Add Map Service" view
    Placeholder text now reflects the selected layer type (from the radio
    buttons). Closes #46

--- www/js/controllers/wms.url.controller.js
+++ www/js/controllers/wms.url.controller.js
@@ -13,6 +13,13 @@
         vm.title = 'wmsUrlController';
         vm.setWMSLayer = setWMSLayer;
         vm.back = back;
+        vm.placeholderText;
+        vm.updatePlaceholder = updatePlaceholder;
+        // placeholder text for different layer types //
+        vm.dynamicPlaceholder = 'http://server.arcgisonline.com/ArcGIS/rest/services/World_Terrain_Base/MapServer/';
+        vm.imagePlaceholder = 'http://sampleserver3.arcgisonline.com/ArcGIS/rest/services/World/MODIS/ImageServer';
+        vm.featurePlaceholder = 'http://services.arcgis.com/rOo16HdIMeOBI4Mb/arcgis/rest/services/stops/FeatureServer/0/';
+        vm.tilePlaceholder = 'http://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png';
 
         $scope.data = {};
 
@@ -23,6 +30,7 @@
         function activate() {
             $scope.data.layerType = 'dynamic';
             $scope.data.opacity = '0.5';
+            vm.placeholderText = vm.dynamicPlaceholder;
         }
 
         function back() {
@@ -31,6 +39,23 @@
 
         function setWMSLayer(wmsInput) {
             wmsUrlService.sendLayerData(wmsInput);
+        }
+
+        function updatePlaceholder(layerType) {
+            switch(layerType) {
+                case 'dynamic':
+                    vm.placeholderText = vm.dynamicPlaceholder;
+                    break;
+                case 'image':
+                    vm.placeholderText = vm.imagePlaceholder;
+                    break;
+                case 'feature':
+                    vm.placeholderText = vm.featurePlaceholder;
+                    break;
+                case 'tile':
+                    vm.placeholderText = vm.tilePlaceholder;
+                    break;
+            }
         }
     }
 })();
